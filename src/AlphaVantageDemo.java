c4bdd586809fc2c8eb909f0b08445fc5352ab13d
Sindhu Krovvidi <sindhukrovvidi@Sindhus-MacBook-Air.local>
2022-10-29 01:57:22 -0400

    implemented dynamic fetch for stocks through API

--- src/AlphaVantageDemo.java
+++ src/AlphaVantageDemo.java
@@ -5,59 +5,41 @@ import java.net.URL;
 import java.awt.dnd.*;
 
 public class AlphaVantageDemo {
-  public static void main(String []args) {
-    //the API key needed to use this web service.
-    //Please get your own free API key here: https://www.alphavantage.co/
-    //Please look at documentation here: https://www.alphavantage.co/documentation/
 
-    // https://www.alphavantage.co/documentation/#symbolsearch -- searches keyword
-    String apiKey = "5VTE6APNI1ZMURXC";
-    String stockSymbol = "GOOG"; //ticker symbol for Google IBM
-    URL url = null;
+  private String apiKey = "5VTE6APNI1ZMURXC";
+//  private String stockSymbol = "GOOG";
+  private URL url = null;
 
+  public void fetchData(String stockSymbol) {
     try {
-      /*
-      create the URL. This is the query to the web service. The query string
-      includes the type of query (DAILY stock prices), stock symbol to be
-      looked up, the API key and the format of the returned
-      data (comma-separated values:csv). This service also supports JSON
-      which you are welcome to use.
-       */
       url = new URL("https://www.alphavantage"
-                    + ".co/query?function=TIME_SERIES_DAILY"
-                    + "&outputsize=full"
-                    + "&symbol"
-                    + "=" + stockSymbol + "&apikey="+apiKey+"&datatype=csv");
-    }
-    catch (MalformedURLException e) {
+          + ".co/query?function=GLOBAL_QUOTE"
+//          + "&outputsize=compact"
+          + "&symbol"
+          + "=" + "IBM" + "&apikey=" + apiKey + "&datatype=csv");
+    } catch (MalformedURLException e) {
       throw new RuntimeException("the alphavantage API has either changed or "
-                                 + "no longer works");
+          + "no longer works");
     }
 
     InputStream in = null;
     StringBuilder output = new StringBuilder();
 
     try {
-      /*
-      Execute this query. This returns an InputStream object.
-      In the csv format, it returns several lines, each line being separated
-      by commas. Each line contains the date, price at opening time, highest
-      price for that date, lowest price for that date, price at closing time
-      and the volume of trade (no. of shares bought/sold) on that date.
-
-      This is printed below.
-       */
       in = url.openStream();
       int b;
 
-      while ((b=in.read())!=-1) {
-        output.append((char)b);
+      while ((b = in.read()) != -1) {
+        output.append((char) b);
       }
-    }
-    catch (IOException e) {
-      throw new IllegalArgumentException("No price data found for "+stockSymbol);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("No price data found for " + stockSymbol);
     }
     System.out.println("Return value: ");
     System.out.println(output.toString());
+  }
+
+  public void searchTickers(String stockSymbol) {
+    fetchData(stockSymbol);
   }
 }
