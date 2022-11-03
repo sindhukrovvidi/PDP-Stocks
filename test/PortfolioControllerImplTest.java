ce30cd46f5d0d9e915ccad6f31eb0a0ed79d9d9c
akhila chiguluri <akhilachiguluri@akhilas-MacBook-Air.local>
2022-11-03 19:01:26 -0400

    added DESIGN_README

---
+++ test/PortfolioControllerImplTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PortfolioControllerImplTest {
+
+  @Test
+  public void addStock() {
+  }
+
+  @Test
+  public void afterAddingStock() {
+  }
+
+  @Test
+  public void viewSpeculate() {
+  }
+
+  @Test
+  public void viewSpeculateHelper() {
+  }
+
+  @Test
+  public void controllerToViewHelper() {
+  }
+}
